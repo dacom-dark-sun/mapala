8fbcbe02aef5a12e62a24587153e3d69768ee803
Dark Sun <dacom.dark.sun@gmail.com>
2016-12-19 00:59:11 +0500

    url rules

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -4,7 +4,8 @@ return [
     'enablePrettyUrl'=>true,
     'showScriptName'=>false,
     'rules'=> [
-        
+                '' => 'site/index',
+       '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
       
     ]
 ];
