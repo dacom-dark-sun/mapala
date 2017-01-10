dfa95aaf6a097a77a3800e49f20ae7200cdb8a58
Dark Sun <dacom.dark.sun@gmail.com>
2017-01-10 14:35:41 +0500

    version fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -45,7 +45,7 @@ $this->beginContent('@frontend/views/layouts/base.php')
     </div>
 <?php $this->endContent() ?>
 
-<div class ="version">alfa 1.01</div>
+<div class ="version">alfa 1.01 <a href="http://mapala.ru">(0-~)</a></div>
 
 <script>
 
