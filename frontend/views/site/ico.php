6342fbd35e4e2c7b9071452eaae0f7ae2dc0b7fd
Dark Sun <dacom.dark.sun@gmail.com>
2017-01-21 02:48:25 +0300

    Zico address fix

--- frontend/views/site/ico.php
+++ frontend/views/site/ico.php
@@ -15,7 +15,7 @@ use common\models\BitCoin;
 <div class="row">
     <div class="col-lg-12">
   
-<div class ='col-xs-3 col-lg-3'>
+<div class ='col-xs-3 col-lg-4'>
                 <div class="panel panel-danger">
                     <div class="panel-heading">   
                         <center>    <?= Yii::t('frontend', 'Personal Bitcoin Address') ?></center>
@@ -40,7 +40,7 @@ use common\models\BitCoin;
     
             </div>
 
-        <div class ='col-xs-6 col-lg-3'>
+        <div class ='col-xs-6 col-lg-2'>
                 <div class="panel panel-danger">
                     <div class="panel-heading">   
                 <center>        <?= Yii::t('frontend', 'Weekly information') ?>  </center>
