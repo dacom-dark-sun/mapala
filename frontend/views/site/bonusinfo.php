73f9e41b3c2743643fb4b67a56997d4e64cecef9
Dark Sun <dacom.dark.sun@gmail.com>
2017-01-21 01:41:57 +0300

    bonus info

---
+++ frontend/views/site/bonusinfo.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<b>Для каждого дня недели действуют бонусы:</b><br><br>
+Суббота:  +6% <br>
+Воскресенье: +5%<br>
+Понедельник: +4%<br>
+Вторник: +3%<br>
+Среда:  +2%<br>
+Четверг: +1%<br>
+Пятница: 0%<br>
