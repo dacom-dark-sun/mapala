26fc9d25849d7ed1a7730e53c111a9422e43eb45
Dark Sun <dacom.dark.sun@gmail.com>
2017-02-03 01:34:17 +0500

    site fix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -53,7 +53,7 @@ class SiteController extends Controller
                 'validatorOptions' => [
                 'maxSize' => 4000000
               ],
-            'url' => 'http://mapala.dev/storage/web/img/', // Directory URL address, where files are stored.
+            'url' => 'https://mapala.net/storage/web/img/', // Directory URL address, where files are stored.
             'path' => '@storage/web/img/' // Or absolute path to directory where files are stored.
             ],
         ];
