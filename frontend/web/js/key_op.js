696f61324dc8bc1db8ba5ba713a55b557927a023
Dark Sun <dacom.dark.sun@gmail.com>
2017-02-04 20:36:40 +0500

    author fix

--- frontend/web/js/key_op.js
+++ frontend/web/js/key_op.js
@@ -323,7 +323,8 @@ window.retry_until_done(doit);
 }
 
 function check_pub_key_golos(pub_key, golos_callback){
- doit = function(){    
+ 
+doit = function(){    
     
  steem.api.getKeyReferences([pub_key], function(err, result) {  //CHANGE TO GOLOS
         golos_callback(err, result);
