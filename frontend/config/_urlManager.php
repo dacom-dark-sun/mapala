a0472e13087ae78817e9add3207e03d75cc53760
Dark Sun <dacom.dark.sun@gmail.com>
2017-02-10 14:24:51 +0500

    comment Url

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -6,7 +6,7 @@ return [
     'rules'=> [
         '/report/<action:index>' => 'report/<action>',
         '/ajax/<action>' => 'ajax/<action>',
-        '/site/<action:add|show_single_blog|ico|stat|team|investors|set-locale|captcha|personal_history|image-upload>' => 'site/<action>',
+        '/site/<action:add|show_single_blog|ico|stat|team|investors|set-locale|captcha|personal_history|comments|image-upload>' => 'site/<action>',
         '/forms/<action>' => 'forms/<action>',
         '/page/<action>' => 'page/<action>',
         '/category/<categories>' => 'site/index',
