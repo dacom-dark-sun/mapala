166001dbfde5137656c277dde453ed6eafeb7b62
Dark Sun <dacom.dark.sun@gmail.com>
2017-02-22 15:54:23 +0500

    fas fix

--- frontend/views/site/faq.php
+++ frontend/views/site/faq.php
@@ -1,3 +1,8 @@
 <?php
-$this->title = 'FAQ';
-echo $body;
+$this->title = 'FAQ';?>
+
+ <div class="row">
+        <div class="col-xs-12 col-lg-8 col-lg-push-2">
+<?= $body;?>
+        </div>
+ </div>
