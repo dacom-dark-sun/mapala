e3f466a550be44dceaf71d89c705f5d2d116fde7
Dark Sun <dacom.dark.sun@gmail.com>
2017-05-26 13:42:46 +0500

    commit

--- frontend/views/site/gbginfo.php
+++ frontend/views/site/gbginfo.php
@@ -4,6 +4,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *  В данный момент существующие объёмы торгов не позволяют быстро конвертировать GBG в BTC. Поэтому мы продаём GBG в частном порядке с коэффициентом 0,85, и при этом ориентируемся на эталонный курс 1 мг золота.')<br>
+В случае появления лучших предложений по выкупу у нас GBG или повышения ликвидности GBG на биржах мы пересмотрим данное правило по конвертации.
  */
 
 ?>
@@ -15,12 +17,5 @@
 <b>3.</b> <?= Yii::t('frontend', 'A few minutes later the transaction will appear in your personal Mapala account;') ?> <br>
 <b>4.</b> <?= Yii::t('frontend','All of our internal accounting is conducted in Bitcoins, so in enrollment investment SBD will be converted on the BTC / SBD rate.') ?><br>
 <br>
-<?php if(\common\models\BlockChain::get_blockchain_from_locale() == 'golos'):?>
-<b> <?= Yii::t('frontend', 'Important!')?> </b> В данный момент существующие объёмы торгов не позволяют быстро конвертировать GBG в BTC. Поэтому мы продаём GBG в частном порядке с коэффициентом 0,85, и при этом ориентируемся на эталонный курс 1 мг золота.')<br>
-В случае появления лучших предложений по выкупу у нас GBG или повышения ликвидности GBG на биржах мы пересмотрим данное правило по конвертации.<br>
-<br>
-<b> Точная формула: </b> Курс_золота_за_тройскую_унцию / 1000 / 31,1034768 / курс BTC * 0,85<br>
-<br>
-<?php ENDIF; ?>
 
 <?= Yii::t('frontend', 'For each day of the week there are bonuses:</b><br><br>Saturday:  +6% <br>Sunday: +5%<br>Monday: +4%<br>Tuesday: +3%<br>Wednesay:  +2%<br>Thursday: +1%<br>Friday: 0%<br>');?>
